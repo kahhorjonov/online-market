cd350c68e55417402ce6c84cccbebc8a992c0998
kahhorjonov <akbarjon3028@gmail.com>
2024-04-02 22:40:40 +0500

    initial commit

---
+++ components/modules/Header/Header.tsx
@@ -0,0 +1,20 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="container header_container">
+        <button className="btn-reset header_burger">
+          <span></span>
+          <span></span>
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
+
+// https://www.figma.com/file/iT1xfXq3NqBNVEF0dSPfW6/%D0%92%D0%B5%D0%B1-%D0%B4%D0%B8%D0%B7%D0%B0%D0%B9%D0%BD-PRO-(Copy)?type=design&node-id=1474-96145&mode=design&t=mb6vVl48uxzlgaDb-0
+// https://rostelecom-shop.vercel.app/
+// https://github.com/skillBlogWebDev/rostelecom-shop
+// https://nextjs.org/docs/pages/api-reference/create-next-app
+// https://www.youtube.com/watch?v=XTY7YNUaAtY
